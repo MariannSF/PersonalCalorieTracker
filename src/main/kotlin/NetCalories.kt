9ebc0bbf07eb48931729c5033b78561a7c7ce39c
hayden <haydencallitsis@hotmail.com>
2022-11-21 20:14:43 -0500

    I created the NetCalories Class which is done mostly but needs to be connected to the dictionary. Once the dictionary is connected some input can be replaced from dictionary values instead of user input.

---
+++ src/main/kotlin/NetCalories.kt
@@ -0,0 +1,32 @@
+//Allowing users to calculate net calories by subtracting
+// the product of all exercises calories expenditure per minute
+// and users exercise time for the
+// exercise from the total of calories consumed from food items.
+import java.util.concurrent.TimeUnit
+
+class NetCalories {
+
+    var caloriesGainedFood = readLine()!!.toDouble()
+    val caloriesLostExercising = readLine()!!.toDouble()
+    var timeSpentExercising = readLine()!!.toLong()
+
+    fun grossCalories(): Double {
+        val grossCalories = caloriesGainedFood - caloriesLostExercising
+        return grossCalories
+    }
+
+    fun productOfAllExercises(): Double {
+        val milliseconds = 60000.0
+        val seconds = TimeUnit.MILLISECONDS.toSeconds(milliseconds.toLong())
+        val productOfAllExercises = grossCalories()/seconds
+        return productOfAllExercises
+    }
+
+    fun liveTimeOfAllExercises(): Double {
+        val productOfAllExercises = productOfAllExercises()
+        timeSpentExercising = TimeUnit.MILLISECONDS.toSeconds(timeSpentExercising)
+        caloriesGainedFood = caloriesGainedFood - timeSpentExercising
+        caloriesGainedFood = caloriesGainedFood - productOfAllExercises
+        return caloriesGainedFood
+    }
+}
