58ae9f7a18c29c13377d96ce65220fe5c095fe86
Jck123 <jck.1.2.3.a@gmail.com>
2022-11-28 16:57:27 -0500

    Cleaned up main

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -68,27 +68,8 @@ fun main() {
                 println("Invalid input detected! Defaulting to N");
         }
     }
-    /*
-    // variable for calories consumed (Maggie)
-    var caloriesConsumed = 0
-    // variable for calories burned (Maggie)
-    var caloriesBurned = 0
-    // variable for user's calorie burning goal (Maggie)
-    // variable for calories still to be burned (Maggie)
-    val caloriesToGo: Int
-    // variable for BMI (Maggie)
-    var bMI = 0.0
 
-    // Getting the user's name
-    println("Enter your Name: ")
-    var userInputName = readLine() // a variable that hold's the user's name
-
-    // input validation (Maggie)
-    while (userInputName.isNullOrEmpty()) {
-        println("Invalid input: Enter name again")
-        userInputName = readLine()
-    }
-    */
+    //Happens after successful login
     while (true) {
         println(
             "What would you like to do?\n" +
@@ -214,7 +195,7 @@ fun main() {
                 // Setting user's calorie goal to their input (Maggie)
                 val goal = (readLine()?.toDoubleOrNull() ?: error("enter a number")).toInt()
                 // Printing the user's calorie goal, so they can see it went through (Maggie)
-                println("Your goal is " + goal + "!")
+                println("Your goal is $goal!")
 
                 // Calculating Calories To Go
                 var caloriesToGo = goal - user1.userTotalBurnedCals
@@ -239,41 +220,6 @@ fun main() {
             "4" -> break;
             else -> println("Invalid input! Please try again")
         }
-
-
-
-
-
-
-
-
-
-
-
-        /*
-
-
-    println("Enter your weight in pounds: ")
-    var userInputW = readLine()?.toDoubleOrNull() ?: error("enter a number")
-
-    // input validation
-    while (userInputW < 0) {
-        println("Invalid input: Enter a Weight that is 0 or Greater")
-        userInputW = readLine()?.toDoubleOrNull() ?: error("enter a number")
-    }
-
-    println("Enter your height in inches: ")
-    var userInputH = readLine()?.toDoubleOrNull() ?: error("enter a number")
-
-    // input validation
-    while (userInputH < 0) {
-        println("Invalid input: Enter a Height that is 0 or Greater")
-        userInputH = readLine()?.toDoubleOrNull() ?: error("enter a number")
-    }
-
-     */
-
         user1.saveToFile()      //Save user data to file
-
     }
 }
